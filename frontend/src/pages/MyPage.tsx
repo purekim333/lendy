e94233ac35d8741e7d38842c1aed5c81b79bb972
오승열 <tmdduf785@naver.com>
2025-10-14 22:40:45 +0900

    initial setting mypage

---
+++ frontend/src/pages/MyPage.tsx
@@ -0,0 +1,8 @@
+export default function MyPage() {
+  return (
+    <div className="min-h-screen flex flex-col bg-white text-gray-800">
+      <h1 className="p-4 text-2xl font-bold">마이페이지</h1>
+      <p className="p-4 text-gray-600">여기는 마이페이지입니다.</p>
+    </div>
+  );
+}
